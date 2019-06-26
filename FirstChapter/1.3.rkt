326c97986882298a18b3894c839fec1580525a1f
sususur <artem002-98@mail.ru>
2019-06-26 19:15:29 +0800

    Refactor 1.3 exercise

--- FirstChapter/1.3.rkt
+++ FirstChapter/1.3.rkt
@@ -1,22 +1,12 @@
 #lang racket
 (require rackunit)
 (define (square x) (* x x))
-(define (larger-square x y z) (if (>= x y)
-                                  (if (>= x z)
-                                      (+ (square x)
-                                         (square y)
-                                          )
-                                      (+ (square x)
-                                         (square z)
-                                      ))
-                                  (if (>= y z)
-                                      (+ (square x)
-                                         (square y)
-                                         )
-                                      (+ (square y)
-                                         (square z)
-                                      ))
-                                  ))
+(define (larger-square x y z) (cond
+                                [(and (>= x y) (>= z y)) (+ (square x) (square z))]
+                                [(and (>= x y) (>= y z)) (+ (square x) (square y))]
+                                [(and (<= x y) (>= z x)) (+ (square y) (square z))]
+                                [else (+ (square x) (square y))]
+                                ))
 
 (check-eq? (larger-square 3 4 2) 25)
 (check-eq? (larger-square 3 3 3) 18)
