88e74de464956dcd7842f5e8d5868a256f7babd8
solar05 <artem00298@gmail.com>
2020-06-16 02:40:40 +0300

    Done 1.15

---
+++ first-chapter/1.15.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+
+; (sine 12.15)
+; (p (sine (4.05)))
+; (p (p (sine (1.35))))
+; (p (p (p (sine (0.45)))))
+; (p (p (p (p (sine (0.15))))))
+; (p (p (p (p (p (sine (0.05)))))))
+;
+; `p' is applied five times.
+; The growth gain is O(log a).
