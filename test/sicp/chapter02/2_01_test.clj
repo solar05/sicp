8bbae7bf3b3c3574252e21f57e3829d5c17508b6
solar05 <artem00298@gmail.com>
2020-12-28 23:54:24 +0300

    Done 2.01

---
+++ test/sicp/chapter02/2_01_test.clj
@@ -0,0 +1,13 @@
+(ns sicp.chapter02.2-01-test
+  (:require [clojure.test :refer :all]
+            [sicp.ratio :refer :all]))
+
+(deftest test-sqrt
+  (testing "Ratio numbers."
+    (is (= [1 2] (make-rat 3 6)))
+    (is (= [1 2] (make-rat -3 -6)))
+    (is (= [-1 2] (make-rat -3 6)))
+    (is (= [-1 2] (make-rat 3 -6)))
+    (is (= [3 1] (make-rat 3)))
+    (is (= "1/2" (str-rat (make-rat 3 6))))
+    (is (= "-1/2" (str-rat (make-rat 3 -6))))))
