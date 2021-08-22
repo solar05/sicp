b2f8a251908ded6d28906dae41ff4b5190c56156
solar05 <artem00298@gmail.com>
2021-08-23 00:43:41 +0300

    Fix test

--- test/sicp/chapter02/2_18_test.clj
+++ test/sicp/chapter02/2_18_test.clj
@@ -3,7 +3,7 @@
             [sicp.chapter02.2-18 :refer :all]
             [sicp.test-helper :refer :all]))
 
-(deftest test-last-pair
+(deftest test-my-reverse
   (assert-equal '(34 149 72 23) (my-reverse '(23 72 149 34)))
   (assert-equal '() (my-reverse '()))
   (assert-equal '(4 3 2 1) (my-reverse '(1 2 3 4))))
