a4cb514eac39a9e7ff13de2e1db8c96fedeaa7fd
solar05 <artem00298@gmail.com>
2021-09-13 01:39:14 +0300

    Fix test

--- test/sicp/chapter02/2_31_test.clj
+++ test/sicp/chapter02/2_31_test.clj
@@ -3,7 +3,7 @@
             [sicp.chapter02.2-31 :as ch]
             [sicp.test-helper :refer :all]))
 
-(deftest test-square-tree
+(deftest test-square-tree-v2
   (let [tree '(1 (2 (3 4) 5) (6 (7 8) (9 10)))
         sq-tree '(1 (4 (9 16) 25) (36 (49 64) (81 100)))]
     (assert-equal sq-tree (ch/square-tree tree))))
