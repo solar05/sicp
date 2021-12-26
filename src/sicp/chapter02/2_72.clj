6f46bc95f706f1a95f81968e2819391f938529c9
solar05 <artem00298@gmail.com>
2021-12-26 03:22:28 +0300

    Done 2.72

---
+++ src/sicp/chapter02/2_72.clj
@@ -0,0 +1,6 @@
+(ns sicp.chapter02.2-72)
+
+; Consider the maximum and minimum
+; The minimum value is the case of a full binary tree O(n * log_n)
+; The maximum value is like in 2.71 is O(n^2)
+; The final answer os between O(n * log_n) and O(n^2)
