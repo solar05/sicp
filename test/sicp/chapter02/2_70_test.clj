9d11bda17db3aeee2861f6c74d108b8fb9d3f0f4
solar05 <artem00298@gmail.com>
2021-12-26 03:14:57 +0300

    Done 2.70

---
+++ test/sicp/chapter02/2_70_test.clj
@@ -0,0 +1,28 @@
+(ns sicp.chapter02.2-70-test
+  (:require [clojure.string :as s]
+            [clojure.test :refer [deftest]]
+            [sicp.chapter02.2-67 :as ch7]
+            [sicp.chapter02.2-68 :as ch8]
+            [sicp.chapter02.2-70 :as ch]
+            [sicp.test-helper :refer [assert-equal]]))
+
+(defonce song-message
+  (->> '(Get a job
+             Sha na na na na na na na na
+             Get a job
+             Sha na na na na na na na na
+             Wah yip yip yip yip yip yip yip yip yip
+             Sha boom)
+       (map (comp symbol s/upper-case))))
+
+(deftest test-rock-songs-alphabet
+  (let [tree ch/rock-songs-alphabet
+        encoded-message (ch8/encode tree song-message)]
+    (assert-equal '(1 1 1 1 1 1 1 1 1 0 1 1 0 0 1 1 1 0 0 0 0 0
+                      0 0 0 0 1 1 1 1 1 1 1 1 1 0 1 1 0 0 1 1 1
+                      0 0 0 0 0 0 0 0 0 1 1 0 1 1 1 0 1 0 1 0 1
+                      0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 1 0)
+                  encoded-message)
+    (assert-equal song-message (ch7/decode encoded-message tree))
+    (assert-equal 84 (count encoded-message))
+    (assert-equal 108 (* 3 (count song-message)))))
