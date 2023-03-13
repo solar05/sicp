9260158a5e5c262daa1ac80badd45d4545006910
solar05 <artem00298@gmail.com>
2023-03-14 01:58:19 +0300

    Add lib, remove obsolete

--- src/sicp/core.clj
+++
@@ -1,5 +0,0 @@
-(ns sicp.core
-  (:gen-class))
-
-(defn -main [& _args]
-  (println "Hello, World!"))
--- test/sicp/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns sicp.core-test
-  (:require [clojure.test :refer :all]
-            [sicp.core :refer :all]))
-
-(deftest a-test
-  (testing "Fixed, no fail."
-    (is (zero? 0))))
