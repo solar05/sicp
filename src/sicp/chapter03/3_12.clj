6a1bb22485b5d23e64ebad0b73e06aec7719e0f8
solar05 <artem00298@gmail.com>
2023-03-25 16:41:27 +0300

    Fix format

--- src/sicp/chapter03/3_12.clj
+++ src/sicp/chapter03/3_12.clj
@@ -1,14 +1,14 @@
-(ns sicp.chapter03.3-12) 
- 
+(ns sicp.chapter03.3-12)
+
 ; z 
 ; => (a b c d) 
- 
+
 ; (cdr x) 
 ; => (b) 
 
 ; w 
 ; => (a b c d) 
- 
+
 ; (cdr x) 
 ; => (b c d) 
 ; besause 'append!' is mutable function when 'append' is not 
